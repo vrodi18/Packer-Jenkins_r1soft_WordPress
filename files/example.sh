e0cdc693fdd9486d28fb791b03f0eea7ae8ae0e5
Vladlen Rodionov <vrodi18@gmail.com>
2020-11-26 07:06:11 +0000

    added MariaDB

---
+++ files/example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo yum install mariadb-server mariadb -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sleep 10
+sudo sed -i s/^SELINUX=.*$/SELINUX=permissive/ /etc/selinux/config
+printf "\n n\n y\n y\n y\n y\n" | mysql_secure_installation
+sleep 10
+sudo mysql -e "CREATE DATABASE wordpress;"
+sleep 10
+sudo mysql -e "GRANT ALL ON wordpress.* TO wpuser@'%' IDENTIFIED BY 'MySecureP@ss';"
+sleep 10
+sudo mysql -e "FLUSH PRIVILEGES ;"
+sudo systemctl restart mariadb
+sudo setenforce 0
